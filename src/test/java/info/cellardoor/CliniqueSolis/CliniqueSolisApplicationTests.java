bedb8de1018cddcf8c1177d2b856bf63ddb98cfc
CeIIardoor <laouinayassine@gmail.com>
2023-04-01 04:10:09 +0000

    Initial commit

---
+++ src/test/java/info/cellardoor/CliniqueSolis/CliniqueSolisApplicationTests.java
@@ -0,0 +1,13 @@
+package info.cellardoor.CliniqueSolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniqueSolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
