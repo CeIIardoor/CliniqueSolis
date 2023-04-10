964bf389881f17c468804855cdf79544da6e8760
CeIIardoor <laouinayassine@gmail.com>
2023-04-10 23:11:45 +0000

    Views V1.0 TODO : Connect to Backend

---
+++ src/main/java/info/cellardoor/CliniqueSolis/Router/Router.java
@@ -0,0 +1,18 @@
+package info.cellardoor.CliniqueSolis.Router;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Router {
+
+    @GetMapping
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboardIndex() {
+        return "dashboard.html";
+    }
+}
