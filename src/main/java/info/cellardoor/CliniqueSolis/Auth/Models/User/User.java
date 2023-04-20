905f249bde2b0f42faffe31d82f7294e5b143af5
CeIIardoor <laouinayassine@gmail.com>
2023-04-20 12:54:22 +0000

    Added defaultUser for login test

--- src/main/java/info/cellardoor/CliniqueSolis/Auth/Models/User/User.java
+++ src/main/java/info/cellardoor/CliniqueSolis/Auth/Models/User/User.java
@@ -49,6 +49,10 @@ public class User implements UserDetails {
         return adminInstance;
     }
 
+    public static User getUserInstance(String nom, String prenom, String email, String mdp) {
+        return new User(nom, prenom, email, new BCryptPasswordEncoder().encode(mdp));
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
