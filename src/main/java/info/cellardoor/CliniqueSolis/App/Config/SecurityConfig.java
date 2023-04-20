4db5b9e04e72e65b98056801d9d18357e3f98ee7
CeIIardoor <laouinayassine@gmail.com>
2023-04-20 22:58:56 +0000

    Fix cors and column name

--- src/main/java/info/cellardoor/CliniqueSolis/App/Config/SecurityConfig.java
+++ src/main/java/info/cellardoor/CliniqueSolis/App/Config/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
