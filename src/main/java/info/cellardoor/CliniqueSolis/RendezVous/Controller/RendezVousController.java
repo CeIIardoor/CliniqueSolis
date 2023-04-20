26d96ab00f4f84e810453cb2b5a1c43cdc2f8a49
Soussan Mehdi <118851400+MehdiKaturipo@users.noreply.github.com>
2023-04-20 02:42:37 +0000

    RDV update

--- src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Controller/RendezVousController.java
+++ src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Controller/RendezVousController.java
@@ -1,10 +1,15 @@
 package info.cellardoor.CliniqueSolis.RendezVous.Controller;
 
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.ListRendezVousResponse;
+import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.RendezVousResponse;
+import info.cellardoor.CliniqueSolis.RendezVous.Http.Request.RendezVousRequest;
+import info.cellardoor.CliniqueSolis.RendezVous.Models.RendezVous;
 import info.cellardoor.CliniqueSolis.RendezVous.Service.RendezVousService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/rendezvous")
@@ -36,5 +41,40 @@ public class RendezVousController {
             return ResponseEntity.badRequest().build();
 
         return ResponseEntity.ok(rendezVousService.getByDate(annee + "-" + String.format("%02d", mois) + "-" + String.format("%02d", jour)));
+    }
+
+    @PostMapping("/createRDV")
+    public ResponseEntity<RendezVousResponse> createRendezVous(@RequestBody RendezVousRequest rendezVousRequest) {
+        RendezVous rendezVous = rendezVousService.createRendezVous(rendezVousRequest);
+        return ResponseEntity.created(URI.create("/api/rendezvous/" + rendezVous.getRendezVousId()))
+                .body(RendezVousResponse.builder()
+                        .rendezVousId(rendezVous.getRendezVousId())
+                        .patientId(rendezVous.getPatient().getPatientId())
+                        .medecinId(rendezVous.getMedecin().getMedecinId())
+                        .date(rendezVous.getDate())
+                        .heure(rendezVous.getHeure())
+                        .duree(rendezVous.getDuree())
+                        .build());
+    }
+    @PutMapping("updateRDV/{id}")
+    public ResponseEntity<RendezVousResponse> updateRendezVous(@PathVariable("id") Integer rendezVousId,
+                                                               @RequestBody RendezVousRequest rendezVousRequest) {
+        RendezVous rendezVous = rendezVousService.updateRendezVous(rendezVousId, rendezVousRequest);
+        if (rendezVous == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(RendezVousResponse.builder()
+                .rendezVousId(rendezVous.getRendezVousId())
+                .patientId(rendezVous.getPatient().getPatientId())
+                .medecinId(rendezVous.getMedecin().getMedecinId())
+                .date(rendezVous.getDate())
+                .heure(rendezVous.getHeure())
+                .duree(rendezVous.getDuree())
+                .build());
+    }
+    @DeleteMapping("deleteRDV/{id}")
+    public ResponseEntity<Void> deleteRendezVous(@PathVariable("id") Integer rendezVousId) {
+        rendezVousService.deleteRendezVous(rendezVousId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Service/RendezVousService.java
+++ src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Service/RendezVousService.java
@@ -1,19 +1,28 @@
 package info.cellardoor.CliniqueSolis.RendezVous.Service;
 
+import info.cellardoor.CliniqueSolis.Medecin.Models.Medecin;
+import info.cellardoor.CliniqueSolis.Medecin.Models.MedecinRepository;
+import info.cellardoor.CliniqueSolis.Patient.Models.Patient;
+import info.cellardoor.CliniqueSolis.Patient.Models.PatientRepository;
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.ListRendezVousResponse;
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.RendezVousResponse;
+import info.cellardoor.CliniqueSolis.RendezVous.Http.Request.RendezVousRequest;
 import info.cellardoor.CliniqueSolis.RendezVous.Models.RendezVous;
 import info.cellardoor.CliniqueSolis.RendezVous.Models.RendezVousRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RendezVousService {
 
     public final RendezVousRepository rendezVousRepository;
+    public final PatientRepository PatientRepository;
+    public final MedecinRepository MedecinRepository;
 
     public ListRendezVousResponse getAll() {
         List<RendezVous> listeRendezVous = rendezVousRepository.findAll();
@@ -44,5 +53,63 @@ public class RendezVousService {
                                 .build())
                         .toList())
                 .build();
+    }
+    private Patient getPatientById(Integer patientId) {
+        Optional<Patient> patient = PatientRepository.findById(patientId);
+        if (patient.isEmpty()) {
+            throw new IllegalArgumentException("Patient not found");
+        }
+        return patient.get();
+    }
+
+    private Medecin getMedecinById(Integer medecinId) {
+        Optional<Medecin> medecin = MedecinRepository.findById(medecinId);
+        if (medecin.isEmpty()) {
+            throw new IllegalArgumentException("Medecin not found");
+        }
+        return medecin.get();
+    }
+
+    public RendezVous createRendezVous(RendezVousRequest rendezVousRequest) {
+        Patient patient = getPatientById(rendezVousRequest.getPatientId());
+        Medecin medecin = getMedecinById(rendezVousRequest.getMedecinId());
+
+        RendezVous rendezVous = RendezVous.builder()
+                .patient(patient)
+                .medecin(medecin)
+                .date(rendezVousRequest.getDate())
+                .heure(rendezVousRequest.getHeure())
+                .duree(rendezVousRequest.getDuree())
+                .build();
+        return rendezVousRepository.save(rendezVous);
+    }
+    public RendezVous updateRendezVous(Integer rendezVousId, RendezVousRequest rendezVousRequest) {
+        Optional<RendezVous> optionalRendezVous = rendezVousRepository.findById(rendezVousId);
+        if (optionalRendezVous.isEmpty()) {
+            return null;
+        }
+        Optional<Patient> patient = PatientRepository.findById(rendezVousRequest.getPatientId());
+        if (patient.isEmpty()) {
+            return null;
+        }
+        Optional<Medecin> medecin = MedecinRepository.findById(rendezVousRequest.getMedecinId());
+        if (medecin.isEmpty()) {
+            return null;
+        }
+        RendezVous rendezVous = optionalRendezVous.get();
+        rendezVous.setPatient(patient.get());
+        rendezVous.setMedecin(medecin.get());
+        rendezVous.setDate(rendezVousRequest.getDate());
+        rendezVous.setHeure(rendezVousRequest.getHeure());
+        rendezVous.setDuree(rendezVousRequest.getDuree());
+        return rendezVousRepository.save(rendezVous);
+    }
+    public void deleteRendezVous(Integer rendezVousId) {
+        Optional<RendezVous> optionalRendezVous = rendezVousRepository.findById(rendezVousId);
+        if (optionalRendezVous.isEmpty()) {
+            throw new EntityNotFoundException("Rendez-vous with id " + rendezVousId + " not found");
+        }
+        RendezVous rendezVous = optionalRendezVous.get();
+        rendezVousRepository.delete(rendezVous);
     }
 }
