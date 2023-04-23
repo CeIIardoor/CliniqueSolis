d1202d937d7dc0026cdea5caafec27e73e32ef18
Soussan Mehdi <118851400+MehdiKaturipo@users.noreply.github.com>
2023-04-23 21:14:53 +0100

    RDV update 2

--- src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Controller/RendezVousController.java
+++ src/main/java/info/cellardoor/CliniqueSolis/RendezVous/Controller/RendezVousController.java
@@ -1,5 +1,8 @@
 package info.cellardoor.CliniqueSolis.RendezVous.Controller;
 
+import info.cellardoor.CliniqueSolis.Medecin.Http.Request.MedecinRequest;
+import info.cellardoor.CliniqueSolis.Medecin.Http.Response.MedecinResponse;
+import info.cellardoor.CliniqueSolis.Medecin.Http.Response.MedecinResponse;
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.ListRendezVousResponse;
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Reponse.RendezVousResponse;
 import info.cellardoor.CliniqueSolis.RendezVous.Http.Request.RendezVousRequest;
@@ -43,37 +46,73 @@ public class RendezVousController {
         return ResponseEntity.ok(rendezVousService.getByDate(annee + "-" + String.format("%02d", mois) + "-" + String.format("%02d", jour)));
     }
 
+    //    @PostMapping("/createRDV")
+//    public ResponseEntity<RendezVousResponse> createRendezVous(@RequestBody RendezVousRequest rendezVousRequest) {
+//        RendezVous rendezVous = rendezVousService.createRendezVous(rendezVousRequest);
+//        return ResponseEntity.ok(RendezVousResponse.builder()
+//                .rendezVousId(rendezVous.getRendezVousId())
+//                .patientId(rendezVous.getPatient().getPatientId())
+//                .medecinId(rendezVous.getMedecin().getMedecinId())
+//                .date(rendezVous.getDate())
+//                .heure(rendezVous.getHeure())
+//                .duree(rendezVous.getDuree())
+//                .build());
+//    }
     @PostMapping("/createRDV")
-    public ResponseEntity<RendezVousResponse> createRendezVous(@RequestBody RendezVousRequest rendezVousRequest) {
-        RendezVous rendezVous = rendezVousService.createRendezVous(rendezVousRequest);
-        return ResponseEntity.ok(RendezVousResponse.builder()
-                .rendezVousId(rendezVous.getRendezVousId())
-                .patientId(rendezVous.getPatient().getPatientId())
-                .medecinId(rendezVous.getMedecin().getMedecinId())
-                .date(rendezVous.getDate())
-                .heure(rendezVous.getHeure())
-                .duree(rendezVous.getDuree())
-                .build());
+    public ResponseEntity<RendezVousResponse> createRendezVous(
+            @RequestBody RendezVousRequest RendezVousRequest
+    ) {
+        return ResponseEntity.ok(rendezVousService.createRendezVous(RendezVousRequest));
     }
-    @PutMapping("updateRDV/{id}")
-    public ResponseEntity<RendezVousResponse> updateRendezVous(@PathVariable("id") Integer rendezVousId,
-                                                               @RequestBody RendezVousRequest rendezVousRequest) {
-        RendezVous rendezVous = rendezVousService.updateRendezVous(rendezVousId, rendezVousRequest);
-        if (rendezVous == null) {
-            return ResponseEntity.notFound().build();
+    @GetMapping("/searchRDV")
+    public ResponseEntity<ListRendezVousResponse> getRDVByDate(
+            @RequestParam(value = "date", required = false) String date) {
+        if (date == null) {
+            return ResponseEntity.ok(rendezVousService.getAll());
         }
-        return ResponseEntity.ok(RendezVousResponse.builder()
-                .rendezVousId(rendezVous.getRendezVousId())
-                .patientId(rendezVous.getPatient().getPatientId())
-                .medecinId(rendezVous.getMedecin().getMedecinId())
-                .date(rendezVous.getDate())
-                .heure(rendezVous.getHeure())
-                .duree(rendezVous.getDuree())
-                .build());
+        return ResponseEntity.ok(rendezVousService.findRendezVousByDate(date));
     }
-    @DeleteMapping("deleteRDV/{id}")
-    public ResponseEntity<Void> deleteRendezVous(@PathVariable("id") Integer rendezVousId) {
-        rendezVousService.deleteRendezVous(rendezVousId);
-        return ResponseEntity.noContent().build();
+    @GetMapping("/{id}")
+    public ResponseEntity<RendezVousResponse> getRendezVousById(
+            @PathVariable("id") Integer id)
+    {
+        return ResponseEntity.ok(rendezVousService.getRendezVousById(id));
     }
+    @PutMapping("/updateRDV/{id}")
+    public ResponseEntity<RendezVousResponse> updateRendezVousnById(
+            @PathVariable("id") Integer id,
+            @RequestBody RendezVousRequest RendezVousRequest
+    ) {
+        return ResponseEntity.ok(rendezVousService.updateRendezVousById(id, RendezVousRequest));
+    }
+    //    @PutMapping("updateRDV/{id}")
+//    public ResponseEntity<RendezVousResponse> updateRendezVous(@PathVariable("id") Integer rendezVousId,
+//                                                               @RequestBody RendezVousRequest rendezVousRequest) {
+//        RendezVous rendezVous = rendezVousService.updateRendezVous(rendezVousId, rendezVousRequest);
+//        if (rendezVous == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(RendezVousResponse.builder()
+//                .rendezVousId(rendezVous.getRendezVousId())
+//                .patientId(rendezVous.getPatient().getPatientId())
+//                .medecinId(rendezVous.getMedecin().getMedecinId())
+//                .date(rendezVous.getDate())
+//                .heure(rendezVous.getHeure())
+//                .duree(rendezVous.getDuree())
+//                .build());
+//    }
+    @DeleteMapping("/deleteRDV/{id}")
+    public ResponseEntity<String> deleteRendezVousById(
+
+            @PathVariable("id") Integer id
+    ) {
+        rendezVousService.deleteRendezVousById(id);
+        return ResponseEntity.ok("Rendez Vous id:" + id + " supprimé");
+    }
+//    @DeleteMapping("deleteRDV/{id}")
+//    public ResponseEntity<String> deleteRendezVous(@PathVariable("id") Integer rendezVousId) {
+//        rendezVousService.deleteRendezVous(rendezVousId);
+//        return ResponseEntity.ok("Rendez-vous "+rendezVousId+" supprimé");
+//    }
+
 }
