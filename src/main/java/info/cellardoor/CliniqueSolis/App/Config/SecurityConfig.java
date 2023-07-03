05bda9558978a9d37727ccc29a1949d5c27d4699
kssiou <mouadkssiou@gmail.com>
2023-07-03 18:11:44 +0100

    fix deny

--- src/main/java/info/cellardoor/CliniqueSolis/App/Config/SecurityConfig.java
+++ src/main/java/info/cellardoor/CliniqueSolis/App/Config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
+                .requestMatchers("/api/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
